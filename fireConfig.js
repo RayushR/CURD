b03e753559b2cfa1ca8ef721623c1a1db4c00d64
@yushDevloper <90122635+RayushR@users.noreply.github.com>
2022-01-09 08:35:08 +0530

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAgghqSLrcqI-i2PKvM1CBV1h0jBI36_2g",
+    authDomain: "my-first-pro-curd.firebaseapp.com",
+    databaseURL: "https://my-first-pro-curd-default-rtdb.firebaseio.com",
+    projectId: "my-first-pro-curd",
+    storageBucket: "my-first-pro-curd.appspot.com",
+    messagingSenderId: "1030915794355",
+    appId: "1:1030915794355:web:a4c61cda697addae0a97c6",
+    measurementId: "G-40K8KS8C7J"
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
